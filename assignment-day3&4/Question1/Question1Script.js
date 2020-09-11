8e699cb380857462ce4a6d4dd606ff5d3e51fefb
Ashish Shukla <70520621+the-ashish@users.noreply.github.com>
2020-09-11 20:57:58 +0530

    day 3-4 assignment

---
+++ assignment-day3&4/Question1/Question1Script.js
@@ -0,0 +1,21 @@
+function changeButton2Image() {
+  const imgEle = document.getElementById('img1');
+  const newUrl =
+    "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/friendliest-dog-breeds-golden-1578596627.jpg?crop=1.00xw:0.752xh;0,0.189xh&resize=980:*";
+
+  imgEle.src = newUrl;
+}
+
+function changeButton3Image() {
+  const imgEle = document.getElementById('img1');
+  const newUrl =
+    "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
+  imgEle.src = newUrl;
+}
+
+function changeButton1Image() {
+  const imgEle = document.getElementById('img1');
+  const newUrl =
+    "https://i.insider.com/5df126b679d7570ad2044f3e?width=900&format=jpeg&auto=webp";
+  imgEle.src = newUrl;
+}
---
+++ assignment-day3&4/Question2/Question2Script.js
@@ -0,0 +1,5 @@
+function changeInputs() {
+  const inpEle1 = document.getElementById('inp1');
+  const inpEle2 = document.getElementById('inp2');
+  inpEle2.value = inpEle1.value;
+}
